760abb604c9a78219d4073f2857e9e1fb36dbacb
Michael Ebner <michael.ebner.14@ucl.ac.uk>
2016-05-15 10:21:04 +0100

    ENH: Suggested WHO values can be computed

--- app/src/main/java/com/github/dailypollute/StatisticalAnalysis.java
+++ app/src/main/java/com/github/dailypollute/StatisticalAnalysis.java
@@ -28,20 +28,28 @@ public class StatisticalAnalysis {
     private double pm10_max = 10;
 
     // WHO recommended guideline values (see http://www.who.int/mediacentre/factsheets/fs313/en/)
-    private double who_pm2_5_annual_mean = 10;  // ug per m3
-    private double who_pm2_5_24h_mean = 25;     // ug per m3
+    private double who_pm2_5_annualMean = 10;  // ug per m3
+    private double who_pm2_5_24hMean = 25;     // ug per m3
 
-    private double who_pm10_annual_mean = 20;   // ug per m3
-    private double who_pm10_24h_mean = 50;      // ug per m3
+    private double who_pm10_annualMean = 20;   // ug per m3
+    private double who_pm10_24hMean = 50;      // ug per m3
 
-    private double who_o3_8h_mean = 100;        // ug per m3
+    private double who_o3_8hMean = 100;        // ug per m3
 
-    private double who_no2_annual_mean = 40;    // ug per m3
-    private double who_no2_1h_mean = 200;       // ug per m3
+    private double who_no2_annualMean = 40;    // ug per m3
+    private double who_no2_1hMean = 200;       // ug per m3
 
     // Computed values in data
-    private double no2_annual_mean = 0;
-    private double no2_1h_mean = 0;
+    private double pm2_5_annualMean = 0;
+    private double pm2_5_24hMean = 0;   
+
+    private double pm10_annualMean = 0; 
+    private double pm10_24hMean = 0;    
+
+    private double o3_8hMean = 0;      
+
+    private double no2_annualMean = 0;
+    private double no2_1hMean = 0;
 
     // Update frequency of PersonalPollutionDataPoint
     private double updateTime = 15;         // minutes
@@ -51,21 +59,161 @@ public class StatisticalAnalysis {
         this.dataPoints = dataPoints;
     }
 
-    //
+    // Analyse data points w.r.t. to WHO recommended figures
     public void analyse() {
-        
+
+        computeRelevantPm2_5Values();
+        computeRelevantPm10Values();
+        computeRelevantO3Values();
         computeRelevantNo2Values();
+
+        System.out.println("PM2.5-levels are: (recommended)");
+        System.out.println("\tannual mean: " + pm2_5_annualMean + " (" + who_pm2_5_annualMean + ")");
+        System.out.println("\t24h mean:     " + pm2_5_24hMean + " (" + who_pm2_5_24hMean + ")");
+
+        System.out.println("PM10-levels are: (recommended)");
+        System.out.println("\tannual mean: " + pm10_annualMean + " (" + who_pm10_annualMean + ")");
+        System.out.println("\t24h mean:     " + pm10_24hMean + " (" + who_pm10_24hMean + ")");
+
+        System.out.println("O3-levels are: (recommended)");
+        System.out.println("\t8h mean:     " + o3_8hMean + " (" + who_o3_8hMean + ")");        
         
         System.out.println("NO2-levels are: (recommended)");
-        System.out.println("\tannual mean: " + no2_annual_mean + " (" + who_no2_annual_mean + ")");
-        System.out.println("\t1h mean:     " + no2_1h_mean + " (" + who_no2_1h_mean + ")");
+        System.out.println("\tannual mean: " + no2_annualMean + " (" + who_no2_annualMean + ")");
+        System.out.println("\t1h mean:     " + no2_1hMean + " (" + who_no2_1hMean + ")");
+
     }
+
+
+    // Get relevant PM2.5 amounts based on the WHO guideline levels
+    private void computeRelevantPm2_5Values(){
+
+        int pm2_5_annualMean_pointnumber = 0;
+        int pm2_5_24hMean_pointnumber = 0;
+
+        // Use this helper to compensate for values in case they are not given
+        double pm2_5_latest_value = 0;
+
+        Date dateNow = new Date();
+        Date dateOneYearBefore = new Date(dateNow.getTime() - TimeUnit.DAYS.toMillis(365)); //subtracts one year
+        Date dateOneDayBefore = new Date(dateNow.getTime() - TimeUnit.HOURS.toMillis(24));  //subtracts 24 hours
+
+        if (dataPoints == null) {
+            throw new RuntimeException("Null dataPoints");
+        }
+
+        // Iterate over all data points to compute WHO recommended values for comparison
+        for (PersonalPollutionDataPoint point : dataPoints) {
+            // Save latest value in case future point will not be given (i.e. use last information in case no value available)
+            if (point.havePm2_5()) {
+                pm2_5_latest_value = point.getPm2_5();
+            }
+            Date date = point.getDate();
+
+            // Annual average
+            if (date.after(dateOneYearBefore)){
+                pm2_5_annualMean += pm2_5_latest_value;
+                pm2_5_annualMean_pointnumber += 1;
+
+                // One hour average
+                if (date.after(dateOneDayBefore)){
+                    pm2_5_24hMean += pm2_5_latest_value;
+                    pm2_5_24hMean_pointnumber += 1;
+                }
+            }
+        }
+
+        // Normalize to get annual and one hour average
+        pm2_5_annualMean /=  (double) pm2_5_annualMean_pointnumber;
+        pm2_5_24hMean /= (double) pm2_5_24hMean_pointnumber;
+    }
+
+
+    // Get relevant PM10 amounts based on the WHO guideline levels
+    private void computeRelevantPm10Values(){
+
+        int pm10_annualMean_pointnumber = 0;
+        int pm10_24hMean_pointnumber = 0;
+
+        // Use this helper to compensate for values in case they are not given
+        double pm10_latest_value = 0;
+
+        Date dateNow = new Date();
+        Date dateOneYearBefore = new Date(dateNow.getTime() - TimeUnit.DAYS.toMillis(365)); //subtracts one year
+        Date dateOneDayBefore = new Date(dateNow.getTime() - TimeUnit.HOURS.toMillis(24));  //subtracts 24 hours
+
+        if (dataPoints == null) {
+            throw new RuntimeException("Null dataPoints");
+        }
+
+        // Iterate over all data points to compute WHO recommended values for comparison
+        for (PersonalPollutionDataPoint point : dataPoints) {
+            // Save latest value in case future point will not be given (i.e. use last information in case no value available)
+            if (point.havePm10()) {
+                pm10_latest_value = point.getPm10();
+            }
+            Date date = point.getDate();
+
+            // Annual average
+            if (date.after(dateOneYearBefore)){
+                pm10_annualMean += pm10_latest_value;
+                pm10_annualMean_pointnumber += 1;
+
+                // One hour average
+                if (date.after(dateOneDayBefore)){
+                    pm10_24hMean += pm10_latest_value;
+                    pm10_24hMean_pointnumber += 1;
+                }
+            }
+        }
+
+        // Normalize to get annual and one hour average
+        pm10_annualMean /=  (double) pm10_annualMean_pointnumber;
+        pm10_24hMean /= (double) pm10_24hMean_pointnumber;
+    }
+
+
+    // Get relevant O3 amounts based on the WHO guideline levels
+    private void computeRelevantO3Values(){
+
+        int o3_8hMean_pointnumber = 0;
+
+        // Use this helper to compensate for values in case they are not given
+        double o3_latest_value = 0;
+
+        Date dateNow = new Date();
+        Date dateEightHoursBefore = new Date(dateNow.getTime() - TimeUnit.HOURS.toMillis(8));  //subtracts one hour
+
+        if (dataPoints == null) {
+            throw new RuntimeException("Null dataPoints");
+        }
+
+        // Iterate over all data points to compute WHO recommended values for comparison
+        for (PersonalPollutionDataPoint point : dataPoints) {
+            // Save latest value in case future point will not be given (i.e. use last information in case no value available)
+            if (point.haveO3()) {
+                o3_latest_value = point.getO3();
+            }
+            Date date = point.getDate();
+
+
+            // Eight hour average
+            if (date.after(dateEightHoursBefore)){
+                o3_8hMean += o3_latest_value;
+                o3_8hMean_pointnumber += 1;
+            }
+        }
+
+        // Normalize to get annual and one hour average
+        o3_8hMean /= (double) o3_8hMean_pointnumber;
+    }
+
 
     // Get relevant NO2 amounts based on the WHO guideline levels
     private void computeRelevantNo2Values(){
 
-        int no2_annual_mean_pointnumber = 0;
-        int no2_1h_mean_pointnumber = 0;
+        int no2_annualMean_pointnumber = 0;
+        int no2_1hMean_pointnumber = 0;
 
         // Use this helper to compensate for values in case they are not given
         double no2_latest_value = 0;
@@ -84,32 +232,52 @@ public class StatisticalAnalysis {
             if (point.haveNo2()) {
                 no2_latest_value = point.getNo2();
             }
-
             Date date = point.getDate();
 
             // Annual average
             if (date.after(dateOneYearBefore)){
-                no2_annual_mean += no2_latest_value;
-                no2_annual_mean_pointnumber += 1;
+                no2_annualMean += no2_latest_value;
+                no2_annualMean_pointnumber += 1;
 
                 // One hour average
                 if (date.after(dateOneHourBefore)){
-                    no2_1h_mean += no2_latest_value;
-                    no2_1h_mean_pointnumber += 1;
+                    no2_1hMean += no2_latest_value;
+                    no2_1hMean_pointnumber += 1;
                 }
             }
         }
 
         // Normalize to get annual and one hour average
-        no2_annual_mean /=  (double) no2_annual_mean_pointnumber;
-        no2_1h_mean /= (double) no2_1h_mean_pointnumber;
+        no2_annualMean /=  (double) no2_annualMean_pointnumber;
+        no2_1hMean /= (double) no2_1hMean_pointnumber;
     }
 
-    public double getNo2_annual_mean() {
-        return no2_annual_mean;
+
+    public double getNo2_annualMean() {
+        return no2_annualMean;
     }
 
-    public double getNo2_1h_mean() {
-        return no2_1h_mean;
+    public double getNo2_1hMean() {
+        return no2_1hMean;
+    }
+
+    public double getO3_8hMean() {
+        return o3_8hMean;
+    }
+
+    public double getPm10_24hMean() {
+        return pm10_24hMean;
+    }
+
+    public double getPm10_annualMean() {
+        return pm10_annualMean;
+    }
+
+    public double getPm2_5_24hMean() {
+        return pm2_5_24hMean;
+    }
+
+    public double getPm2_5_annualMean() {
+        return pm2_5_annualMean;
     }
 }
--- app/src/test/java/com/github/dailypollute/ExampleUnitTest.java
+++ app/src/test/java/com/github/dailypollute/ExampleUnitTest.java
@@ -14,30 +14,37 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
-
-    @Test
-    public void myFirstTest() throws Exception {
+    public void computeRelevantWhoValues() throws Exception {
 
         List<PersonalPollutionDataPoint> dataPoints = new ArrayList<PersonalPollutionDataPoint>();
         Date date_now = new Date();
 
         int sampling_time = 6; // in min, i.e. sampling every 6 minutes
-        double pollution_hour = (60./sampling_time-1)/2.;
-        double pollution_year = (60./sampling_time*24*365-1)/2.;
+        
+        double pollution_pm2_5_24hMean = (60./sampling_time*24-1)/2.;
+        double pollution_pm2_5_annualMean = (60./sampling_time*24*365-1)/2.;
 
-        int N_points = 365*24*60/sampling_time-1; //At least that to
+        double pollution_pm10_24hMean = (60./sampling_time*24-1)/2.;
+        double pollution_pm10_annualMean = (60./sampling_time*24*365-1)/2.;
+
+        double pollution_no2_1hMean = (60./sampling_time*1-1)/2.;
+        double pollution_no2_annualMean = (60./sampling_time*24*365-1)/2.;
+        
+        double pollution_o3_8hMean = (60./sampling_time*8-1)/2.;
+
+        int N_points = 365*24*60/sampling_time-1; //At least that number to have sampled on entire year
 
         for (int i=N_points; i>=0; i--){
             PersonalPollutionDataPoint dataPoint = new PersonalPollutionDataPoint();
 
             // Specify time "of request"
-            Date date = new Date(date_now.getTime() - TimeUnit.MINUTES.toMillis(i*6));
+            Date date = new Date(date_now.getTime() - TimeUnit.MINUTES.toMillis(i*sampling_time));
 
             // Feed data point with fetched data
+            dataPoint.setO3((double) i);
             dataPoint.setNo2((double) i);
+            dataPoint.setPm2_5((double) i);
+            dataPoint.setPm10((double) i);
             dataPoint.setDate(date);
 
             // Add point to personal list
@@ -48,8 +55,15 @@ public class ExampleUnitTest {
         statisticalAnalysis.setDataPoints(dataPoints);
         statisticalAnalysis.analyse();
 
-        assertEquals(pollution_hour, statisticalAnalysis.getNo2_1h_mean(), 1e-8);
-        assertEquals(pollution_year, statisticalAnalysis.getNo2_annual_mean(), 1e-8);
-    }
+        assertEquals(pollution_pm2_5_24hMean, statisticalAnalysis.getPm2_5_24hMean(), 1e-8);
+        assertEquals(pollution_pm2_5_annualMean, statisticalAnalysis.getPm2_5_annualMean(), 1e-8);
 
+        assertEquals(pollution_pm10_24hMean, statisticalAnalysis.getPm10_24hMean(), 1e-8);
+        assertEquals(pollution_pm10_annualMean, statisticalAnalysis.getPm10_annualMean(), 1e-8);
+
+        assertEquals(pollution_no2_1hMean, statisticalAnalysis.getNo2_1hMean(), 1e-8);
+        assertEquals(pollution_no2_annualMean, statisticalAnalysis.getNo2_annualMean(), 1e-8);
+
+        assertEquals(pollution_o3_8hMean, statisticalAnalysis.getO3_8hMean(), 1e-8);
+    }
 }
